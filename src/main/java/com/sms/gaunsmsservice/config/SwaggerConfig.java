150d40d6b6ddd6b68d78dbcb66a2fbd98935a57e
sahinbinici <sahin863@outlook.com>
2025-02-22 01:24:31 +0300

    swagger-ui çalıştırıldı

--- src/main/java/com/sms/gaunsmsservice/config/SwaggerConfig.java
+++ src/main/java/com/sms/gaunsmsservice/config/SwaggerConfig.java
@@ -1,8 +1,29 @@
 package com.sms.gaunsmsservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-    // Ekstra yapılandırma gerekirse buraya ekle
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "basicAuth";
+        
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")))
+                .info(new Info()
+                        .title("GAUN SMS Service API")
+                        .version("1.0")
+                        .description("SMS API Dokümantasyonu"));
+    }
 } 
